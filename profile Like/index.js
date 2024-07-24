bcaa842beaada03ac2bdeff701f816e5953e4640
Abhay5981 <abhaykum78@gmail.com>
2024-07-24 18:44:28 +0530

    first commit

---
+++ profile Like/index.js
@@ -0,0 +1,10 @@
+const heartEls = document.getElementById("#heart")
+
+// for (let heartEl of heartEls) {
+    heartEls.addEventListener("onClick", () => {
+        console.log("move");
+    })
+// }
+
+
+// heartEls()
